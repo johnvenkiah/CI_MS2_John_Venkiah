de6225edd6905abfc49a92cadd09943b0343a5e3
johnvenkiah <contact@johnvenkiah.com>
2021-06-01 21:12:04 +0000

    add hamburger to other pages via js-file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,36 @@
+let navBar = `
+<nav class="navbar">
+    <a href="index.html" class="nav-logo">
+        <h1>Musical Minds</h1>
+    </a>
+    <ul class="nav-menu">
+        <li class="nav-item">
+            <a href="index.html" class="nav-link">Home</a>
+        </li>
+        <li class="nav-item">
+            <a href="play.html" class="nav-link">Play</a>
+        </li>
+        <li class="nav-item">
+            <a href="quiz.html" class="nav-link">Quiz</a>
+        </li>
+        <li class="nav-item">
+            <a href="learn.html" class="nav-link">Learn</a>
+        </li>
+        <li class="nav-item">
+            <a href="contact.html" class="nav-link">Contact</a>
+        </li>
+    </ul>
+    <button class="hamburger">
+        <span class="bar"></span>
+        <span class="bar"></span>
+        <span class="bar"></span>
+    </button>
+</nav>
+`;
+
+let header = document.getElementsByClassName('header')[0];
+header.innerHTML = navBar;
+
 let hamburger = document.querySelector(".hamburger");
 let navMenu = document.querySelector(".nav-menu");
 
