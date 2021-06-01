10ea9bceacd3d19262c0a61afd93f2b5afd230ad
johnvenkiah <contact@johnvenkiah.com>
2021-06-01 12:03:46 +0000

    Add hamburger menu to quiz.html.

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+let hamburger = document.querySelector(".hamburger");
+let navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+// So hamburger closes when link is clicked
+
+let navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
