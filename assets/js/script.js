9b765d7ed233ba0104e0bad9919d4679a114f2ca
johnvenkiah <contact@johnvenkiah.com>
2021-06-02 14:28:57 +0000

    add play functionality to piano keys

--- assets/js/script.js
+++ assets/js/script.js
@@ -57,3 +57,46 @@ function closeMenu() {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }
+
+/**
+ * The piano, which responds to user input. Borrowed some code here from Kyle at
+ * Web Dev Simplified through this great video: https://youtu.be/vjco5yKZpU8
+ */
+
+const WHITE_KEYS = ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'",]
+const BLACK_KEYS = ['w', 'e', 't', 'y', 'u', 'o', 'p', '[',]
+
+//Create a variable for all keys
+const keys = document.querySelectorAll('.key')
+const whiteKeys = document.querySelectorAll('.key.white')
+const blackKeys = document.querySelectorAll('.key.black')
+
+/*Add event listener function when keys are clicked to play note function.
+Arrow function to minimize written code. */
+
+keys.forEach(key => {
+    key.addEventListener('click', () => playNote(key))
+})
+
+document.addEventListener('keydown', e => {
+    if (e.repeat) return
+    const key = e.key
+    const whiteKeyIndex = WHITE_KEYS.indexOf(key)
+    const blackKeyIndex = BLACK_KEYS.indexOf(key)
+
+    if (whiteKeyIndex > -1) playNote(whiteKeys[whiteKeyIndex])
+    if (blackKeyIndex > -1) playNote(blackKeys[blackKeyIndex])
+})
+
+/**
+ * Play note function,
+ */
+function playNote(key) {
+    const noteAudio  = document.getElementById(key.dataset.note)
+    noteAudio.currentTime = 0
+    noteAudio.play()
+    key.classList.add('active')
+    document.addEventListener('mouseup', () => {
+        key.classList.remove('active')
+    })
+}
