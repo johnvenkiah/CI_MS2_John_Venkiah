6d2d33d42a4b7c7478edf79d5fb45b3e995d83b8
johnvenkiah <contact@johnvenkiah.com>
2021-06-03 19:01:41 +0000

    add more qustions

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -313,11 +313,243 @@
   },
   {
     "question": "Name this note!",
-    "image": "e-flat-4.png",
-    "wrong-1": "B",
-    "wrong-2": "Ab",
-    "wrong-3": "C",
+    "image": "e-flat-5.png",
+    "wrong-1": "G",
+    "wrong-2": "C",
+    "wrong-3": "F",
     "correct": "Eb"
+  },
+  {
+    "question": "Name this note!",
+    "image": "f-3.png",
+    "wrong-1": "G",
+    "wrong-2": "A",
+    "wrong-3": "C",
+    "correct": "F"
+  },
+  {
+    "question": "Name this note!",
+    "image": "f-4.png",
+    "wrong-1": "D",
+    "wrong-2": "G",
+    "wrong-3": "Ab",
+    "correct": "F"
+  },
+  {
+    "question": "Name this note!",
+    "image": "f-5.png",
+    "wrong-1": "D",
+    "wrong-2": "B",
+    "wrong-3": "Cb",
+    "correct": "F"
+  },
+  {
+    "question": "Name this note!",
+    "image": "f-sharp-3.png",
+    "wrong-1": "A#",
+    "wrong-2": "G",
+    "wrong-3": "C",
+    "correct": "F#"
+  },
+  {
+    "question": "Name this note!",
+    "image": "f-sharp-4.png",
+    "wrong-1": "F",
+    "wrong-2": "C",
+    "wrong-3": "C#",
+    "correct": "F#"
+  },
+  {
+    "question": "Name this note!",
+    "image": "f-sharp-5.png",
+    "wrong-1": "Bb",
+    "wrong-2": "C#",
+    "wrong-3": "G",
+    "correct": "F#"
+  },
+  {
+    "question": "Name this note!",
+    "image": "g-3.png",
+    "wrong-1": "E",
+    "wrong-2": "D",
+    "wrong-3": "F",
+    "correct": "G"
+  },
+  {
+    "question": "Name this note!",
+    "image": "g-4.png",
+    "wrong-1": "D",
+    "wrong-2": "Db",
+    "wrong-3": "G#",
+    "correct": "G"
+  },
+  {
+    "question": "Name this note!",
+    "image": "g-5.png",
+    "wrong-1": "B",
+    "wrong-2": "E",
+    "wrong-3": "F",
+    "correct": "G"
+  },
+  {
+    "question": "Name this note!",
+    "image": "g-flat-3.png",
+    "wrong-1": "F",
+    "wrong-2": "A#",
+    "wrong-3": "B",
+    "correct": "Gb"
+  },
+  {
+    "question": "Name this note!",
+    "image": "g-flat-4.png",
+    "wrong-1": "G",
+    "wrong-2": "A",
+    "wrong-3": "F",
+    "correct": "Gb"
+  },
+  {
+    "question": "Name this note!",
+    "image": "g-flat-5.png",
+    "wrong-1": "D",
+    "wrong-2": "Bb",
+    "wrong-3": "F#",
+    "correct": "Gb"
+  },
+  {
+    "question": "Name this note!",
+    "image": "g-sharp-3.png",
+    "wrong-1": "F",
+    "wrong-2": "B",
+    "wrong-3": "F#",
+    "correct": "G#"
+  },
+  {
+    "question": "Name this note!",
+    "image": "g-sharp-4.png",
+    "wrong-1": "D",
+    "wrong-2": "A",
+    "wrong-3": "Eb",
+    "correct": "G#"
+  },
+  {
+    "question": "Name this note!",
+    "image": "g-sharp-5.png",
+    "wrong-1": "A#",
+    "wrong-2": "D",
+    "wrong-3": "Gb",
+    "correct": "G#"
+  },
+  {
+    "question": "Name this symbol!",
+    "image": "halfnote-rest.png",
+    "wrong-1": "16th rest",
+    "wrong-2": "D",
+    "wrong-3": "Whole-note rest",
+    "correct": "Half-note rest"
+  },
+  {
+    "question": "Name the duration of this note!",
+    "image": "halfnote.png",
+    "wrong-1": "10th note",
+    "wrong-2": "12th note",
+    "wrong-3": "Whole-note",
+    "correct": "Half-note"
+  },
+  {
+    "question": "Name this note!",
+    "image": "low-a-flat.png",
+    "wrong-1": "A#",
+    "wrong-2": "G",
+    "wrong-3": "E",
+    "correct": "Ab"
+  },
+  {
+    "question": "Name this note!",
+    "image": "low-a-sharp.png",
+    "wrong-1": "G",
+    "wrong-2": "Bb",
+    "wrong-3": "F",
+    "correct": "A#"
+  },
+  {
+    "question": "Name this note!",
+    "image": "low-a.png",
+    "wrong-1": "B",
+    "wrong-2": "Eb",
+    "wrong-3": "Ab",
+    "correct": "A"
+  },
+  {
+    "question": "Name this note!",
+    "image": "low-b-flat.png",
+    "wrong-1": "F",
+    "wrong-2": "C",
+    "wrong-3": "G#",
+    "correct": "Bb"
+  },
+  {
+    "question": "Name this note!",
+    "image": "low-b.png",
+    "wrong-1": "D",
+    "wrong-2": "E#",
+    "wrong-3": "F",
+    "correct": "B"
+  },
+  {
+    "question": "Name this note!",
+    "image": "low-g-flat.png",
+    "wrong-1": "F",
+    "wrong-2": "C",
+    "wrong-3": "B#",
+    "correct": "Gb"
+  },
+  {
+    "question": "Name this note!",
+    "image": "low-g-sharp.png",
+    "wrong-1": "Ab",
+    "wrong-2": "F",
+    "wrong-3": "C",
+    "correct": "G#"
+  },
+  {
+    "question": "Name this note!",
+    "image": "low-g.png",
+    "wrong-1": "Gb",
+    "wrong-2": "C",
+    "wrong-3": "E",
+    "correct": "G"
+  },
+  {
+    "question": "Name this symbol!",
+    "image": "quarter-rest.png",
+    "wrong-1": "Half-note rest",
+    "wrong-2": "8th-rest",
+    "wrong-3": "16th-rest",
+    "correct": "Quarter-note rest"
+  },
+  {
+    "question": "Name the duration of this note!",
+    "image": "quarter.png",
+    "wrong-1": "Half-note",
+    "wrong-2": "5th",
+    "wrong-3": "16th",
+    "correct": "Quarter-note"
+  },
+  {
+    "question": "Name this symbol!",
+    "image": "wholenote-rest.png",
+    "wrong-1": "8th-rest",
+    "wrong-2": "5th-rest",
+    "wrong-3": "F",
+    "correct": "Whole-note rest"
+  },
+  {
+    "question": "Name the duration of this note!",
+    "image": "wholenote.png",
+    "wrong-1": "3rd-rest",
+    "wrong-2": "16th-rest",
+    "wrong-3": "F",
+    "correct": "Whole-note rest"
   },
 ]
 
