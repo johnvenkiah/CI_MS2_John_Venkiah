6fb60342712ac152b3ba321be48842940075be42
johnvenkiah <contact@johnvenkiah.com>
2021-06-08 11:35:30 +0000

    work moe on form functionality!

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -3,21 +3,22 @@ let errorMsg = document.querySelector('#error-msg');
 
 function validateForm(event) {
   event.preventDefault();
-  let username = document.querySelector('#name').value;
-  let email = document.querySelector('#email').value;
-  // let allowedEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  let from_name = document.querySelector('#name').value;
+  let from_email = document.querySelector('#email').value;
+  let allowedEmail = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
   
-  if (username !== ""
-  //  || email.value.match(allowedEmail)
-   ) 
-   {
+  if (from_name !== "") {
       emailjs.init("user_xdb9Fr0JUjCoYFOs4hiu4");
       emailjs.sendForm('gmail_jvenkiah', 'musical_minds', contactForm);
-          console.log('SUCCESS!');
+      console.log('Form sent sucessfully!');
+      errorMsg.innerHTML == `
+        Hi ${from_name} , your message has been sent.
+      `;
+
   } else {
     errorMsg.innerHTML = `
     Please fill in a valid Email adress and a name!
-  `
+  `;
     console.log('FAILED...', errorMsg);
   }
 }
