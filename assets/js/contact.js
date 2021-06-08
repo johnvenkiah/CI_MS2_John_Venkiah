e7038d7064e8b0b833dd014cd8c27e6248b27f2e
johnvenkiah <contact@johnvenkiah.com>
2021-06-08 08:18:36 +0000

    Create logic for form

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -9,16 +9,15 @@ function validateForm(event) {
   let username = document.querySelector('#name').value;
   let email = document.querySelector('#email').value;
   let allowedEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-  if (username = "" || email !== allowedEmail) {
-    submitForm();
-  }
-  else {
+  
+  if (username !== "" || email.value.match(allowedEmail)) {
+      emailjs.init("user_xdb9Fr0JUjCoYFOs4hiu4");
+      emailjs.sendForm('contact_service', 'contact_form', this);
+          console.log('SUCCESS!');
+  } else {
     errorMsg.innerHTML = `
     Please fill in a valid Email adress and a name!
-  `;
+  `
+    console.log('FAILED...', errorMsg);
   }
-}
-
-function submitForm() {
-  
 }
