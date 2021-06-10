e8fbcbbb965b3c24015cdc3c2aced1ce51ab7aa2
johnvenkiah <contact@johnvenkiah.com>
2021-06-10 12:08:32 +0000

    Add comments on contact.js

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -9,10 +9,13 @@ const contactForm = document.querySelector('.contact-form')
 
 //message to user once message is sent to confirm or to display error
 const sentMsg = document.querySelector('#sent-msg');
+
+//name and email entered by user
 const from_name = document.querySelector('#name');
 const from_email = document.querySelector('#email');
-//regex taken from w3 resource to validate email adresses
-//at https://www.w3resource.com/javascript/form/email-validation.php
+
+/* regex for valdation taken from w3 resource to validate email adresses
+at https://www.w3resource.com/javascript/form/email-validation.php */
 const allowedEmail = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
 
 //Validation fired when clicking the "send" button
