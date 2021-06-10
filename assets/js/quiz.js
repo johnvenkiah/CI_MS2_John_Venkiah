940a2da0e4f8ceb6b3539f4c2d54b24ad0f2767b
johnvenkiah <contact@johnvenkiah.com>
2021-06-10 12:49:49 +0000

    create new logic for quiz, testing

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -5,27 +5,42 @@ var answersBox = document.querySelector('#answers-box');
 var staffBox = document.querySelector('#staffbox');
 var questionsRandom = Math.floor(Math.random() * questions.length);
 
-function randomAnswer(min, max) {
-    min = Math.ceil(3);
-    max = Math.floor(7);
-    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
-  }
+// function randomAnswer(min, max) {
+//     min = Math.ceil(3);
+//     max = Math.floor(7);
+//     return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+//   }
+
 
 function playQuiz() {
     
-    answersBox.innerHTML = `
-    <button class="quiz-button" id="answer-1">${questions[questionsRandom].a}</button>
-    <button class="quiz-button" id="answer-2">${questions[questionsRandom].b}</button>
-    <button class="quiz-button" id="answer-3">${questions[questionsRandom].c}</button>
-    <button class="quiz-button" id="answer-4">${questions[questionsRandom].correct}</button>
-    `;
+    // start timer, score = 0
+
 
     newQuestion();
 }
 
+var buttons = new Array()
+    buttons[0] = `<button onclick="newQuestion()" class="quiz-button" id="answer-1">${questions[questionsRandom].a}</button>`;
+    buttons[1] = `<button onclick="newQuestion()" class="quiz-button" id="answer-1">${questions[questionsRandom].b}</button>`;
+    buttons[2] = `<button onclick="newQuestion()" class="quiz-button" id="answer-1">${questions[questionsRandom].c}</button>`;
+    buttons[3] = `<button onclick="newQuestion()" class="quiz-button" id="answer-1">${questions[questionsRandom].correct}</button>`;
+
 function newQuestion() {
     staffBox.innerHTML = `
     <img src="assets/images/quiz/${questions[questionsRandom].image}" alt="Current question">
-    `
+    `;
 
+    var i = 0;
+    var random;
+
+    while (i < buttons.length) {
+        random = Math.floor(Math.random() * buttons.length);
+
+        if (buttons[random] !="selected") {
+            document.answersBox.write = (buttons[random]);
+            buttons[random] = 'selected';
+            i++;
+        }
+    }
 }
