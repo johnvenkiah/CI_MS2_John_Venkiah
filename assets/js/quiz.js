e7b25473d14d3cf98edc663194fbbce03edb0104
johnvenkiah <contact@johnvenkiah.com>
2021-06-12 09:57:01 +0000

    add remove modal function to close button, yet to be implemented

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -18,7 +18,7 @@ function playQuiz() {
                     <h2>Well Done!</h2>
                     <h3 class="subheading-other">You scored ${document.getElementById('current-score').innerText}!</h3>
                     <button onclick="playQuiz()" id="play-button" class="quiz-button">Play Again</button>
-                    <button id="close-button">Close</button>
+                    <button id="close-button" onclick="removeModal()">Close</button>
                 </div>
             </div>
         `;
