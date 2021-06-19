b472fbfe5fb4b0f053f2fba54fcd4802a7a78104
johnvenkiah <contact@johnvenkiah.com>
2021-06-19 23:56:10 +0000

    add jquery and learn.js

---
+++ assets/js/learn.js
@@ -0,0 +1,17 @@
+document.onload(function() {
+
+  let key = 'AIzaSyBKMQZlfKuvbhmyF99jz5wNmF1A1x7jFZM';
+  let eTag = 'DgDalgcFj4FYHL1ylkD8gAGENw4';
+  let URL = 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=15&order=rating&q=music%20theory%20beginners&relevanceLanguage=en&key=[YOUR_API_KEY] HTTP/1.1';
+
+  let details = {
+    part: 'snippet',
+    key: key,
+    maxResults: 20,
+  }
+
+  function loadVideo() {
+    
+  }
+
+});
