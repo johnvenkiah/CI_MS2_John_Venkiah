8ecb5c47bc16355ffb9571cdb27a6a041c3de40b
johnvenkiah <contact@johnvenkiah.com>
2021-06-20 22:40:23 +0000

    add videoList function and created json-file extention fro api list

--- assets/js/learn.js
+++ assets/js/learn.js
@@ -1,12 +1,12 @@
-let key = 'AIzaSyCENLiRl8jnjs8Nc0kkDXEltYIuhqoNfeE';
+let KEY = 'AIzaSyCENLiRl8jnjs8Nc0kkDXEltYIuhqoNfeE';
 
 let options = {
   part: 'snippet',
-  key: key,
+  key: KEY,
   maxResults: 15
 }
 
-fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=music%20theory%20beginners%20piano%20sight%20reading&relevanceLanguage=en&key=${key}`)
+fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=music%20theory%20beginners%20piano%20sight%20reading&relevanceLanguage=en&key=${KEY}`)
   .then(response => response.json())
   .then(data => {
     document.querySelector('#learn-text').innerText = data;
@@ -15,13 +15,26 @@ fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=
     console.log(data);
 
     mainVideo(vidId, vidTitle);
+    videosList();
 })
 
 function mainVideo(vidId, vidTitle) {
-  let mainVidHtml = `<iframe src="https://www.youtube.com/embed/${vidId}" title="${vidTitle}" 
-  frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; 
-  gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+  let mainVidHtml = `<iframe src="https://www.youtube.com/embed/${vidId}" title="${vidTitle}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
 
     document.querySelector('#main-video').innerHTML = mainVidHtml;
     console.log(vidId);
+
 }
+
+function videosList() {
+  let vidListHtml = `<article>
+    <img class="thumbn" src="assets/images/learn/thumbn_videos/thumb1.webp"></img>
+    <div class="video-info">
+      <h3>Title</h3>
+      <p>Description</p>
+    </div>
+  </article>`
+
+  let videosContainer = document.querySelector('#videos-section');
+  videosContainer.innerHTML = vidListHtml;
+  }
