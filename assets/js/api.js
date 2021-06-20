8250d8e602325b0fa8fba609dfb202506cefe741
johnvenkiah <contact@johnvenkiah.com>
2021-06-20 22:10:36 +0000

    add more code for api and first video at top from api

---
+++ assets/js/api.js
@@ -0,0 +1,352 @@
+{
+  "kind": "youtube#searchListResponse",
+  "etag": "LJpG3hCsYEKkMY_vlt8_ysL6nvQ",
+  "nextPageToken": "CAoQAA",
+  "regionCode": "SE",
+  "pageInfo": {
+    "totalResults": 848533,
+    "resultsPerPage": 10
+  },
+  "items": [
+    {
+      "kind": "youtube#searchResult",
+      "etag": "Ocg9AhDTb37mMG0zglRk95UrcHw",
+      "id": {
+        "kind": "youtube#video",
+        "videoId": "PKLlZoK0JTc"
+      },
+      "snippet": {
+        "publishedAt": "2019-01-01T23:05:23Z",
+        "channelId": "UCvVqPUvDUDmqz1vZYGhGEoA",
+        "title": "30 min of Basic Piano Sight Reading Practice (Grade 1)",
+        "description": "Practice piano sight reading with this progressive set of exercises. Before you start playing you have time to prepare. We explain here how to prepare. If the gaps ...",
+        "thumbnails": {
+          "default": {
+            "url": "https://i.ytimg.com/vi/PKLlZoK0JTc/default.jpg",
+            "width": 120,
+            "height": 90
+          },
+          "medium": {
+            "url": "https://i.ytimg.com/vi/PKLlZoK0JTc/mqdefault.jpg",
+            "width": 320,
+            "height": 180
+          },
+          "high": {
+            "url": "https://i.ytimg.com/vi/PKLlZoK0JTc/hqdefault.jpg",
+            "width": 480,
+            "height": 360
+          }
+        },
+        "channelTitle": "musicwithnopain",
+        "liveBroadcastContent": "none",
+        "publishTime": "2019-01-01T23:05:23Z"
+      }
+    },
+    {
+      "kind": "youtube#searchResult",
+      "etag": "Fad5wIVZDa6RUNMQy-MBcGO_YhQ",
+      "id": {
+        "kind": "youtube#video",
+        "videoId": "gEI7uYOCQXo"
+      },
+      "snippet": {
+        "publishedAt": "2019-03-19T21:37:44Z",
+        "channelId": "UC_DmCvOP5Q_eBMRDvqqRXjg",
+        "title": "How To Read Notes (Beginner Piano Lesson)",
+        "description": "Sight-reading has never come easily to me. I've struggled with it from the beginning. At one point in my early piano years, I had to repeat an entire level because ...",
+        "thumbnails": {
+          "default": {
+            "url": "https://i.ytimg.com/vi/gEI7uYOCQXo/default.jpg",
+            "width": 120,
+            "height": 90
+          },
+          "medium": {
+            "url": "https://i.ytimg.com/vi/gEI7uYOCQXo/mqdefault.jpg",
+            "width": 320,
+            "height": 180
+          },
+          "high": {
+            "url": "https://i.ytimg.com/vi/gEI7uYOCQXo/hqdefault.jpg",
+            "width": 480,
+            "height": 360
+          }
+        },
+        "channelTitle": "Pianote",
+        "liveBroadcastContent": "none",
+        "publishTime": "2019-03-19T21:37:44Z"
+      }
+    },
+    {
+      "kind": "youtube#searchResult",
+      "etag": "XmqvVmP8RJqRCzaNJQLFL9rsQzE",
+      "id": {
+        "kind": "youtube#video",
+        "videoId": "yjhZUPa2J78"
+      },
+      "snippet": {
+        "publishedAt": "2016-06-17T20:55:04Z",
+        "channelId": "UCvVqPUvDUDmqz1vZYGhGEoA",
+        "title": "Improve your Piano Sight Reading for Absolute Beginners - Grade 1",
+        "description": "Practice sight reading on the piano or keyboard. You will have progressive exercises to prepare you to the British standard of Grade 1 sight reading (ABRSM, ...",
+        "thumbnails": {
+          "default": {
+            "url": "https://i.ytimg.com/vi/yjhZUPa2J78/default.jpg",
+            "width": 120,
+            "height": 90
+          },
+          "medium": {
+            "url": "https://i.ytimg.com/vi/yjhZUPa2J78/mqdefault.jpg",
+            "width": 320,
+            "height": 180
+          },
+          "high": {
+            "url": "https://i.ytimg.com/vi/yjhZUPa2J78/hqdefault.jpg",
+            "width": 480,
+            "height": 360
+          }
+        },
+        "channelTitle": "musicwithnopain",
+        "liveBroadcastContent": "none",
+        "publishTime": "2016-06-17T20:55:04Z"
+      }
+    },
+    {
+      "kind": "youtube#searchResult",
+      "etag": "xLlZoot5xfHKH3eoqrPI0DAVV7g",
+      "id": {
+        "kind": "youtube#video",
+        "videoId": "zRnl30L4mnU"
+      },
+      "snippet": {
+        "publishedAt": "2017-02-01T22:08:59Z",
+        "channelId": "UCvVqPUvDUDmqz1vZYGhGEoA",
+        "title": "Piano Sight Reading Part 1- Grade 1 for Beginners",
+        "description": "Practice sight reading on the piano or keyboard. You will have progressive exercises to prepare you to the British standard of Grade 1 sight reading (ABRSM, ...",
+        "thumbnails": {
+          "default": {
+            "url": "https://i.ytimg.com/vi/zRnl30L4mnU/default.jpg",
+            "width": 120,
+            "height": 90
+          },
+          "medium": {
+            "url": "https://i.ytimg.com/vi/zRnl30L4mnU/mqdefault.jpg",
+            "width": 320,
+            "height": 180
+          },
+          "high": {
+            "url": "https://i.ytimg.com/vi/zRnl30L4mnU/hqdefault.jpg",
+            "width": 480,
+            "height": 360
+          }
+        },
+        "channelTitle": "musicwithnopain",
+        "liveBroadcastContent": "none",
+        "publishTime": "2017-02-01T22:08:59Z"
+      }
+    },
+    {
+      "kind": "youtube#searchResult",
+      "etag": "xQjMnoZGnOW-yeig6hAykL5ISV4",
+      "id": {
+        "kind": "youtube#video",
+        "videoId": "jSOU-J9KHbg"
+      },
+      "snippet": {
+        "publishedAt": "2017-11-05T05:39:10Z",
+        "channelId": "UCnlxlDrSSk_2PyLW_7qKRcg",
+        "title": "How to Read Notes Fast - The Landmark System",
+        "description": "In this video I teach you how to read notes fast, using The Landmark System. Note Recognition is the first step to sight reading music / scores / pieces. By using ...",
+        "thumbnails": {
+          "default": {
+            "url": "https://i.ytimg.com/vi/jSOU-J9KHbg/default.jpg",
+            "width": 120,
+            "height": 90
+          },
+          "medium": {
+            "url": "https://i.ytimg.com/vi/jSOU-J9KHbg/mqdefault.jpg",
+            "width": 320,
+            "height": 180
+          },
+          "high": {
+            "url": "https://i.ytimg.com/vi/jSOU-J9KHbg/hqdefault.jpg",
+            "width": 480,
+            "height": 360
+          }
+        },
+        "channelTitle": "Musicians Inspired",
+        "liveBroadcastContent": "none",
+        "publishTime": "2017-11-05T05:39:10Z"
+      }
+    },
+    {
+      "kind": "youtube#searchResult",
+      "etag": "b1q8c0iUSGeeV9UCAAC3HqVPbWI",
+      "id": {
+        "kind": "youtube#video",
+        "videoId": "gKKMtst_B4Y"
+      },
+      "snippet": {
+        "publishedAt": "2014-01-19T00:04:23Z",
+        "channelId": "UCidvz_O5zdX7z2mr4zkeL2g",
+        "title": "Piano Sight-Reading Lesson 1",
+        "description": "The music staff is simply the piano standing up sideways! These lessons assume no prior knowledge of reading music. Added subtitles, use the tool to choose ...",
+        "thumbnails": {
+          "default": {
+            "url": "https://i.ytimg.com/vi/gKKMtst_B4Y/default.jpg",
+            "width": 120,
+            "height": 90
+          },
+          "medium": {
+            "url": "https://i.ytimg.com/vi/gKKMtst_B4Y/mqdefault.jpg",
+            "width": 320,
+            "height": 180
+          },
+          "high": {
+            "url": "https://i.ytimg.com/vi/gKKMtst_B4Y/hqdefault.jpg",
+            "width": 480,
+            "height": 360
+          }
+        },
+        "channelTitle": "Jane",
+        "liveBroadcastContent": "none",
+        "publishTime": "2014-01-19T00:04:23Z"
+      }
+    },
+    {
+      "kind": "youtube#searchResult",
+      "etag": "SMg-UyiqtIpZBwSm3e5xEM01xT4",
+      "id": {
+        "kind": "youtube#video",
+        "videoId": "3A6NcbSJpHU"
+      },
+      "snippet": {
+        "publishedAt": "2020-11-26T23:03:09Z",
+        "channelId": "UCgFVXRPV07XjUAwZbNF45lQ",
+        "title": "The Piano Sight Reading Tricks That Make Reading Music 100% Easier",
+        "description": "If you are a beginner or intermediate music student who struggles with sight reading at the piano, the chances are you have only been using an outdated version ...",
+        "thumbnails": {
+          "default": {
+            "url": "https://i.ytimg.com/vi/3A6NcbSJpHU/default.jpg",
+            "width": 120,
+            "height": 90
+          },
+          "medium": {
+            "url": "https://i.ytimg.com/vi/3A6NcbSJpHU/mqdefault.jpg",
+            "width": 320,
+            "height": 180
+          },
+          "high": {
+            "url": "https://i.ytimg.com/vi/3A6NcbSJpHU/hqdefault.jpg",
+            "width": 480,
+            "height": 360
+          }
+        },
+        "channelTitle": "LeCheileMusic",
+        "liveBroadcastContent": "none",
+        "publishTime": "2020-11-26T23:03:09Z"
+      }
+    },
+    {
+      "kind": "youtube#searchResult",
+      "etag": "mOSMaMovug65-eug9t7jLXFKZCw",
+      "id": {
+        "kind": "youtube#video",
+        "videoId": "N6lt3wBSQJ0"
+      },
+      "snippet": {
+        "publishedAt": "2010-07-21T02:41:08Z",
+        "channelId": "UCBhhg12d5l2iSQ-UpyVgqvQ",
+        "title": "Piano Sight Reading Lesson Part 1 of 32",
+        "description": "Visit http://www.shawncheek.com to try some more free videos, and if you like them you can buy a membership and get access to all ONE HUNDRED AND ...",
+        "thumbnails": {
+          "default": {
+            "url": "https://i.ytimg.com/vi/N6lt3wBSQJ0/default.jpg",
+            "width": 120,
+            "height": 90
+          },
+          "medium": {
+            "url": "https://i.ytimg.com/vi/N6lt3wBSQJ0/mqdefault.jpg",
+            "width": 320,
+            "height": 180
+          },
+          "high": {
+            "url": "https://i.ytimg.com/vi/N6lt3wBSQJ0/hqdefault.jpg",
+            "width": 480,
+            "height": 360
+          }
+        },
+        "channelTitle": "shawn cheek",
+        "liveBroadcastContent": "none",
+        "publishTime": "2010-07-21T02:41:08Z"
+      }
+    },
+    {
+      "kind": "youtube#searchResult",
+      "etag": "2mXsYUyfd5VUjbVmNhL13TJWal0",
+      "id": {
+        "kind": "youtube#video",
+        "videoId": "Y0l9wVILwI8"
+      },
+      "snippet": {
+        "publishedAt": "2020-08-10T14:45:27Z",
+        "channelId": "UCJCQQIPzVfLpZTfiKIP3KQg",
+        "title": "Explaining How I Sight-read Music 📖🤓♫ | Tiffany Vlogs #125",
+        "description": "http://www.togetherwithclassical.org/support Music Theory Books: For piano beginners: love how cute Alfred's kids series is ...",
+        "thumbnails": {
+          "default": {
+            "url": "https://i.ytimg.com/vi/Y0l9wVILwI8/default.jpg",
+            "width": 120,
+            "height": 90
+          },
+          "medium": {
+            "url": "https://i.ytimg.com/vi/Y0l9wVILwI8/mqdefault.jpg",
+            "width": 320,
+            "height": 180
+          },
+          "high": {
+            "url": "https://i.ytimg.com/vi/Y0l9wVILwI8/hqdefault.jpg",
+            "width": 480,
+            "height": 360
+          }
+        },
+        "channelTitle": "Tiffany Poon",
+        "liveBroadcastContent": "none",
+        "publishTime": "2020-08-10T14:45:27Z"
+      }
+    },
+    {
+      "kind": "youtube#searchResult",
+      "etag": "M9tAF4apsy49JAzyrVlSdpAjwEw",
+      "id": {
+        "kind": "youtube#video",
+        "videoId": "wJSQ9t0nG3Q"
+      },
+      "snippet": {
+        "publishedAt": "2016-09-16T18:00:05Z",
+        "channelId": "UCAh-jIhQPq0XVuu3RN4zGaw",
+        "title": "How to Read Sheet Music in One Easy Lesson",
+        "description": "Today I will teach you how to read sheet music notes in this one easy lesson. This is a complete lesson with both instruction and practice. If you are a beginner ...",
+        "thumbnails": {
+          "default": {
+            "url": "https://i.ytimg.com/vi/wJSQ9t0nG3Q/default.jpg",
+            "width": 120,
+            "height": 90
+          },
+          "medium": {
+            "url": "https://i.ytimg.com/vi/wJSQ9t0nG3Q/mqdefault.jpg",
+            "width": 320,
+            "height": 180
+          },
+          "high": {
+            "url": "https://i.ytimg.com/vi/wJSQ9t0nG3Q/hqdefault.jpg",
+            "width": 480,
+            "height": 360
+          }
+        },
+        "channelTitle": "Piano Lessons On The Web",
+        "liveBroadcastContent": "none",
+        "publishTime": "2016-09-16T18:00:05Z"
+      }
+    }
+  ]
+}
--- assets/js/learn.js
+++ assets/js/learn.js
@@ -1,7 +1,27 @@
 let key = 'AIzaSyCENLiRl8jnjs8Nc0kkDXEltYIuhqoNfeE';
 
-fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=15&order=rating&q=piano%20music%20theory%20beginners%20sight%20reading&relevanceLanguage=en&safeSearch=moderate&key=${key}`)
-  .then(response => response.text())
+let options = {
+  part: 'snippet',
+  key: key,
+  maxResults: 15
+}
+
+fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=music%20theory%20beginners%20piano%20sight%20reading&relevanceLanguage=en&key=${key}`)
+  .then(response => response.json())
   .then(data => {
     document.querySelector('#learn-text').innerText = data;
-  })
+    var vidId = data.items[0].id.videoId;
+    var vidTitle = data.items[0].snippet.title;
+    console.log(data);
+
+    mainVideo(vidId, vidTitle);
+})
+
+function mainVideo(vidId, vidTitle) {
+  let mainVidHtml = `<iframe src="https://www.youtube.com/embed/${vidId}" title="${vidTitle}" 
+  frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; 
+  gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+
+    document.querySelector('#main-video').innerHTML = mainVidHtml;
+    console.log(vidId);
+}
