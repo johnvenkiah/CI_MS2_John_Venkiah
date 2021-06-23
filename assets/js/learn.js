9520391676fe937b4003549b73fbb3c0e8627aa9
johnvenkiah <contact@johnvenkiah.com>
2021-06-23 13:31:44 +0000

    js: cors problem still there, solution not working.

--- assets/js/learn.js
+++ assets/js/learn.js
@@ -27,22 +27,6 @@ fetch(myRequest, myInit).then(response => response.json())
   videosList(data);
 });
 
-// let options = {
-//   part: 'snippet',
-//   key: KEY,
-//   maxResults: 10
-// };
-
-// fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=music%20theory%20beginners%20piano%20sight%20reading&relevanceLanguage=en&key=${KEY}`, {mode: 'cors'})
-//   .then(response => response.json())
-//   .then(data => {
-//     var vidId = data.items[0].id.videoId;
-//     var vidTitle = data.items[0].snippet.title;   // fix this so title shows for each video
-
-//     mainVideo(vidId, vidTitle);
-//     videosList(data);
-//   });
-
 function mainVideo(vidId, vidTitle) {
   let mainVidHtml = `<iframe src="https://www.youtube.com/embed/${vidId}" title="${vidTitle}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
 
