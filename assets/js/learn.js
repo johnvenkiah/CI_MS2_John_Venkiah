f5f81de7e84e2f5af748c0b752f096cc14b6c1f4
johnvenkiah <contact@johnvenkiah.com>
2021-06-24 01:19:37 +0000

    js: comment out redundand code

--- assets/js/learn.js
+++ assets/js/learn.js
@@ -3,30 +3,26 @@ let navbar = document.querySelector('.navbar');
 
 navbar.style.boxShadow = '2px 0 4px #2a2a2a';
 
-var cors_api_host = 'cors-anywhere.herokuapp.com';
-var cors_api_url = 'https://' + cors_api_host + '/';
+// var cors_api_host = 'cors-anywhere.herokuapp.com';
+// var cors_api_url = 'https://' + cors_api_host + '/';
 
-(function() {
-  var slice = [].slice;
-  var origin = window.location.protocol + '//' + window.location.host;
-  var open = XMLHttpRequest.prototype.open;
-  XMLHttpRequest.prototype.open = function() {
-      var args = slice.call(arguments);
-      var targetOrigin = /^https?:\/\/([^\/]+)/i.exec(args[1]);
-      if (targetOrigin && targetOrigin[0].toLowerCase() !== origin &&
-          targetOrigin[1] !== cors_api_host) {
-          args[1] = cors_api_url + args[1];
-      }
-      return open.apply(this, args);
-  };
-  console.log('cors setup complete');
-})();
+// (function() {
+//   var slice = [].slice;
+//   var origin = window.location.protocol + '//' + window.location.host;
+//   var open = XMLHttpRequest.prototype.open;
+//   XMLHttpRequest.prototype.open = function() {
+//       var args = slice.call(arguments);
+//       var targetOrigin = /^https?:\/\/([^\/]+)/i.exec(args[1]);
+//       if (targetOrigin && targetOrigin[0].toLowerCase() !== origin &&
+//           targetOrigin[1] !== cors_api_host) {
+//           args[1] = cors_api_url + args[1];
+//       }
+//       return open.apply(this, args);
+//   };
+//   console.log('cors setup complete');
+// })();
 
-
-
-let myRequest = new Request(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=music%20theory%20beginners%20piano%20sight%20reading&relevanceLanguage=en&key=${KEY}`);
-
-fetch(myRequest)
+fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=music%20theory%20beginners%20piano%20sight%20reading&relevanceLanguage=en&key=${KEY}`)
   .then(response => response.json())
   .then(data => {
     var vidId = data.items[0].id.videoId;
